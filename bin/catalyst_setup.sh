f6dfe6656ec3b1d5cd586f92fb74a13de2354b5d
Lucas Ramage <ramage.lucas@protonmail.com>
2021-11-06 13:59:59 +0000

    Fetch snapshot instead of creating
    Closes: https://github.com/oxr463/gentoo-dotfiles/issues/2

--- bin/catalyst_setup.sh
+++ bin/catalyst_setup.sh
@@ -1,33 +1,46 @@
 #!/bin/sh
 set -e
 
-DISTFILES_LATEST_URL="https://distfiles.gentoo.org/releases/amd64/autobuilds"
+DISTFILES_URL="https://distfiles.gentoo.org"
+AUTOBUILDS_URL="${DISTFILES_URL}/releases/amd64/autobuilds"
 
-DEFAULT_CATALYST_TMP="/var/tmp/catalyst/builds/default"
+CATALYST_TMP="/var/tmp/catalyst"
+CATALYST_BUILDS="${CATALYST_TMP}/builds/default"
+CATALYST_SNAPSHOTS="${CATALYST_TMP}/snapshots"
 
-create_catalyst_tmp() {
-  mkdir -p "${DEFAULT_CATALYST_TMP}"
+DISTDIR="/var/cache/distfiles"
+
+create_catalyst_dirs() {
+  mkdir -p "${CATALYST_BUILDS}"
+  mkdir -p "${CATALYST_SNAPSHOTS}"
 }
 
 get_seed_tarball() {
-  latest_stage3=$(curl -sqL "${DISTFILES_LATEST_URL}/latest-stage3-amd64.txt" | \
+  latest_stage3=$(curl -sqL "${AUTOBUILDS_URL}/latest-stage3-amd64.txt" | \
     awk '!/^#/ { print $1 }') # do not print comment lines
 
-  curl -L "${DISTFILES_LATEST_URL}/${latest_stage3}" -o "${DEFAULT_CATALYST_TMP}/stage3-amd64-latest.tar.xz"
+  curl -L "${AUTOBUILDS_URL}/${latest_stage3}" \
+    -o "${CATALYST_BUILDS_DEFAULT}/stage3-amd64-latest.tar.xz"
 }
 
 create_portage_snapshot() {
+  # FIXME: doesn't work in docker
   catalyst -s "$(date +%Y.%m.%d)"
 }
 
+get_portage_snapshot() {
+  curl -L "${DISTFILES_URL}/snapshots/portage-latest.tar.xz" \
+    -o "${CATALYST_SNAPSHOTS}/gentoo-latest.tar.xz"
+}
+
 create_distfiles() {
-  mkdir -p /var/cache/distfiles
+  mkdir -p "${DISTDIR}"
 }
 
 main() {
-  create_catalyst_tmp
+  create_catalyst_dirs
   get_seed_tarball &
-  create_portage_snapshot &
+  get_portage_snapshot &
   create_distfiles
 
   wait
